96a4acaff8da49e075369e2fa458119a71118212
Anna Vahtera <anna@vahtera.org>
2025-02-25 13:25:55 +0200

    Added translation support. Language included: English, Finnish.
    Added command line argument and help display.

--- Scramble.py
+++ Scramble.py
@@ -1,24 +1,55 @@
 # Word Scramble Game
-# Version: 1.0.1
+# Version: 2.1.4
 # Written by: Anna Vahtera
-# Last Updated: 2021-10-07
-# This program is a word scramble game. The program reads a list of words from a file, selects a random word,
-# shuffles the letters, and asks the user to guess the word. The user has a limited number of turns to guess the word.
-# The program then reveals the correct word and the points scored by the user.
+# Last Updated: 2025-02-20
+# This program is a word scramble game. The program reads a list of words from a file and scrambles the word.
+# The players have a limited number of turns to guess the word. The player gets points for guessing the word correctly.
 # The program uses ANSI escape codes to color the text.
-
-from json.scanner import NUMBER_RE
 import random
 import string
 import sys
 from os import system
+
+# Initialize Variables
 random.seed()
-fileName = "english.lst" # File Name to Read the Words from
 turns = 3 # Number of turns to play
 points = 5 # Default Points for Correct Answer on First Try
 arguments = len(sys.argv) # Get the Command Line Arguments
 iTurn = 0 # Player Turn
 iRnd = 0 # Round Number
+
+def displayHelp(): # Display Help Screen
+    print("\n\nInstructions:\n")
+    print("Usage: Scramble.py [#players] [#rounds] [language]\n")
+    print("Language is any of the following:")
+    print("--english: English language. (Default)")
+    print("--finnish: Finnish language.")
+    print("\nCommand line arguments:")
+    print("-h, --help: this Help screen.")
+    #print("-r, --rules: Display rules.")
+    print("-p#: Number of players.")
+    print("-r#: Number of rounds.")
+    print("\nIf selecting the language, make sure the required language file is present [\"language.lst\"].\n")
+    exit()
+
+
+def setMode(): # Set Program Language Mode
+    t = "english"
+    if arguments > 1:
+        for l in range(1, arguments):
+            tStr = sys.argv[l]
+            if tStr == "--finnish":
+                t = "finnish"
+            elif tStr == "--english":
+                t = "english"
+            elif tStr == "-h" or tStr == "--help":
+                displayHelp()
+            #elif tStr == "-r" or tStr == "--rules":
+            #    displayRules()
+    return t
+
+lang = setMode() # Default Language
+fileName = lang + ".lst" # File Name to Read the Words from
 
 # Color Definitions
 WHITE = "\033[37m" # White Text Color
@@ -33,23 +64,137 @@ BOLD = "\033[1m" # Bold Text
 NOBOLD = "\033[22m" # No Bold Text
 ENDC = "\033[0m" # Reset Text Color
 
+# Translatable strings:
+text = {
+  "play-again?": {
+    "english": "Play another round? Y/N [Y]: ",
+    "finnish": "Pelataanko uusi kierros? K/E [K]: ",
+  },
+  "quit": {
+    "english": "Thank you for playing!",
+    "finnish": "Kiitos, että pelasit!",
+  },
+  "players": {
+    "english": " players",
+    "finnish": " pelaajaa",
+  },
+  "player": {
+    "english": " Player ",
+    "finnish": " Pelaaja ",
+  },
+  "lang": {
+    "english": "English",
+    "finnish": "Suomi",
+  },
+  "language": {
+    "english": "Language",
+    "finnish": "Kieli",
+  },
+  "wordlist": {
+    "english": "Wordlist",
+    "finnish": "Sanalista",
+  },
+    "nPlayers": {
+    "english": "\n  Enter the number of players [1]: ",
+    "finnish": "\n  Syötä pelaajien määrä [1]: ",
+  },
+    "nRounds": {
+    "english": "\n  Enter the number of rounds [1]: ",
+    "finnish": "\n  Syötä kierrosten määrä [1]: ",
+  },
+    "scrWord": {
+    "english": "\n  Scrambled Word: ",
+    "finnish": "\n  Sekoitettu Sana: ",
+  },
+    "continue": {
+    "english": "  Press Enter to Continue...",
+    "finnish": "  Paina Enter jatkaaksesi...",
+  },
+    "incorrect": {
+    "english": "\n\n  Incorrect. ",
+    "finnish": "\n\n  Väärin. ",
+  },
+    "correct": {
+    "english": "\n\n  Correct!",
+    "finnish": "\n\n  Oikein!",
+  },
+    "correctword": {
+    "english": "The correct word is: ",
+    "finnish": "Oikea sana on: ",
+  },
+    "correctwordbegins": {
+    "english": "The correct word begins with:",
+    "finnish": "Oikea sana alkaa:",
+  },
+    "scored": {
+    "english": "  You scored ",
+    "finnish": "  Sait ",
+  },
+    "points": {
+    "english": " points!\n",
+    "finnish": " pistettä!\n",
+  },
+    "youhave": {
+    "english": " You have ",
+    "finnish": " Sinulla on ",
+  },
+    "turnsleft": {
+    "english": " turns to guess!",
+    "finnish": " kierrosta aikaa arvata!",
+  },
+    "guessword": {
+    "english": "\n  Guess the word!",
+    "finnish": "\n  Arvaa sana!",
+  },
+    "thisis": {
+    "english": " This is your ",
+    "finnish": " Tämä on ",
+  },
+    "last": {
+    "english": "last",
+    "finnish": "viimeinen",
+  },
+    "turntoguess": {
+    "english": " turn to guess!",
+    "finnish": " kierroksesi arvata!",
+  },
+    "word": {
+    "english": " / Word ",
+    "finnish": " / Sana ",
+  },
+    "guess": {
+    "english": ", Guess ",
+    "finnish": ", Arvaus ",
+  },
+    "finalscores": {
+    "english": "\n Final Scores for the game:\n",
+    "finnish": "\n Pelin lopulliset pisteet:\n",
+  },
+    "fscored": {
+    "english": " scored: ",
+    "finnish": " sai: ",
+},
+}
+
 def SetPlayers(): # Function to get the number of Players
+    r = 1 # Default to 1 Player
     if arguments > 1:
         for l in range(1, arguments):
             if sys.argv[l][:2] == "-p":
                 r = sys.argv[l][2:]
     else:
-        r = input("Enter the number of players [1]: ") or 1
+        r = input(text["nPlayers"][lang]) or 1
     
     return int(r)
 
 def SetRounds(): # Function to get the number of rounds to play
+    r = 1 # Default to 1
     if arguments > 1:
         for l in range(1, arguments):
             if sys.argv[l][:2] == "-r":
                 r = sys.argv[l][2:]
     else:
-        r = input("Enter the number of rounds [1]: ") or 1
+        r = input(text["nRounds"][lang]) or 1
     
     return int(r)
 
@@ -62,6 +207,7 @@ def shuffle_word(word): # Function to Shuffle the Word
 with open(fileName, "r", encoding="utf-8") as f: # Open the File and Read the Lines into an Array
     arrWord = [line.strip() for line in f]
 
+system('cls') # Clear the screen
 numPlayers = SetPlayers() # Get the number of players
 numRounds = SetRounds() # Get the number of rounds
 
@@ -75,36 +221,36 @@ def GameRound(plrTurn, plrRnd): # Function to Play a Round of the Game
     global playerPoints
     global iTurn
 
-    print(BOLD + BLACK + "\n  Scrambled Word: " + CYAN, scrWord.lower() + ENDC) # Print the scrambled word
+    print(BOLD + BLACK + text["scrWord"][lang] + CYAN, scrWord.lower() + ENDC) # Print the scrambled word
 
     for l in range(turns): # Loop through the number of turns
-        print(BOLD + GREEN + "\n  Guess the word!" + ENDC + " You have " + YELLOW + str(3 - l) + ENDC + " turns to guess!") # Print the game title
-        inputWord = input("\n\n  Player " + str(plrTurn + 1) + " / Word " + str(plrRnd + 1) + ", Guess " + str(l + 1) + ": " + WHITE) # Get the user input
+        print(BOLD + GREEN + text["guessword"][lang] + ENDC + text["youhave"][lang] + YELLOW + str(3 - l) + ENDC + text["turnsleft"][lang]) # Print the game title
+        inputWord = input("\n " + text["player"][lang] + str(plrTurn + 1) + text["word"][lang] + str(plrRnd + 1) + text["guess"][lang] + str(l + 1) + ": " + WHITE) # Get the user input
 
         if inputWord.lower() == ansWord.lower(): # Check if the input word is the same as the answer
-            print( GREEN + "\n\n  Correct!\n" + ENDC) # Print correct if the input is correct
-            print("  You scored " + YELLOW + str(points) + ENDC + " points!\n") # Print the points
-            go = input("  Press Enter to Continue...") # Wait for the user to press Enter")
+            print( GREEN + text["correct"][lang] + "\n" + ENDC) # Print correct if the input is correct
+            print(text["scored"][lang] + YELLOW + str(points) + ENDC + text["points"][lang]) # Print the points
+            go = input(text["continue"][lang]) # Wait for the user to press Enter")
             break
         else:
-            print(RED + "\n\n  Incorrect. " + ENDC + "The correct word begins with:" + GREEN, ansWord[:l + 1].capitalize() + ENDC) # Print the correct word if the input is incorrect
+            print(RED + text["incorrect"][lang] + ENDC + text["correctwordbegins"][lang] + GREEN, ansWord[:l + 1].capitalize() + ENDC) # Print the correct word if the input is incorrect
             points -= 1 
     
     if inputWord.lower() != ansWord.lower():
-        print(BOLD + GREEN + "\n  Guess the word!" + ENDC + " This is your " + YELLOW + "last" + ENDC + " turn to guess!") # Print the game title
+        print(BOLD + GREEN + text["guessword"][lang] + ENDC + text["thisis"][lang] + YELLOW + text["last"][lang] + ENDC + text["turntoguess"][lang]) # Print the game title
         inputWord = input("\n\n  Guess " + str(turns + 1) + ": " + WHITE) # Get the user input
         if inputWord.lower() == ansWord.lower(): # Check if the input word is the same as the answer
-            print( GREEN + "\n\n  Correct!" + ENDC) # Print correct if the input is correct
+            print( GREEN + text["correct"][lang] + ENDC) # Print correct if the input is correct
         else:
-            print(RED + "\n\n  Incorrect. " + ENDC + "The correct word is: " + GREEN, ansWord.capitalize() + ENDC) # Print the correct word
+            print(RED + text["incorrect"][lang] + ENDC + text["correctword"][lang] + GREEN, ansWord.capitalize() + ENDC) # Print the correct word
             points = 0
-            go = input("  Press Enter to Continue...") # Wait for the user to press Enter")
+            go = input(text["continue"][lang]) # Wait for the user to press Enter")
 
 # Set all player's points to 0 to start
 playerPoints = [0] * numPlayers
 
-for iTurn in range(numPlayers):
-    for iRnd in range(numRounds):
+for iTurn in range(numPlayers): # Loop through the number of players
+    for iRnd in range(numRounds): # Loop through the number of rounds
         system('cls') # Clear the screen
         r = random.randint(0, len(arrWord) - 1) # Randomly select a word from the list
         ansWord = arrWord[r] # Get the word from the list
@@ -113,9 +259,9 @@ for iTurn in range(numPlayers):
         GameRound(iTurn, iRnd) # Play a Round of the Game
         playerPoints[iTurn] += points # Add the points to the player's total points
 
-# Final Scores
+# Display Final Scores
 system('cls') # Clear the screen
-print("  Final Scores for the game:\n") # Print a newline
+print(text["finalscores"][lang]) # Print a newline
 for n in range(numPlayers):
-    print("  Player " + str(n + 1) + " scored: " + YELLOW + str(playerPoints[n]) + ENDC + " points!") # Print the points
+    print(text["player"][lang] + str(n + 1) + text["fscored"][lang] + YELLOW + str(playerPoints[n]) + ENDC + text["points"][lang]) # Print the points
 print("\n") # Print a newline
